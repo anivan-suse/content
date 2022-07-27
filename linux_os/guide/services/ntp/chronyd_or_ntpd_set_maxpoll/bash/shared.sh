20b331d24a2db098af45733c8b152df1c50ee409
anivan-suse <anastasija.ivanovic@suse.com>
2022-07-27 14:29:31 +0200

    Ansible remediation was only changing chrony.conf and not chrony.d/pool.conf

--- linux_os/guide/services/ntp/chronyd_or_ntpd_set_maxpoll/bash/shared.sh
+++ linux_os/guide/services/ntp/chronyd_or_ntpd_set_maxpoll/bash/shared.sh
@@ -2,21 +2,27 @@
 
 {{{ bash_instantiate_variables("var_time_service_set_maxpoll") }}}
 
-
-{{% if 'ubuntu' not in product %}}
-pof="/usr/sbin/pidof"
+{{% if ('sle' in product) or ('ubuntu' in product) %}}
+    pof="/bin/pidof"
 {{% else %}}
-pof="/bin/pidof"
+    pof="/usr/sbin/pidof"
 {{% endif %}}
 
-config_file="/etc/ntp.conf"
-$pof ntpd || config_file="{{{ chrony_conf_path }}}"
+# get list of ntp files
+CONFIG_FILES="/etc/ntp.conf"
+$pof ntpd || {
+    CONFIG_FILES=$(grep -rnl "^\(server\|pool\|peer\)" /etc/chrony* | awk -F : '{print $1}')
+}
 
-
-# Set maxpoll values to var_time_service_set_maxpoll
-sed -i "s/^\(\(server\|pool\|peer\).*maxpoll\) [0-9][0-9]*\(.*\)$/\1 $var_time_service_set_maxpoll \3/" "$config_file"
-
-# Add maxpoll to server, pool or peer entries without maxpoll
-grep "^\(server\|pool\|peer\)" "$config_file" | grep -v maxpoll | while read -r line ; do
+for config_file in $CONFIG_FILES; do
+    # Add maxpoll to server, pool or peer entries without maxpoll
+    grep "^\(server\|pool\|peer\)" "$config_file" | grep -v maxpoll | while read -r line ; do
         sed -i "s/$line/& maxpoll $var_time_service_set_maxpoll/" "$config_file"
+    done
 done
+
+for config_file in $CONFIG_FILES; do
+    # Set maxpoll values to var_time_service_set_maxpoll
+    sed -i "s/^\(\(server\|pool\|peer\).*maxpoll\) [0-9][0-9]*\(.*\)$/\1 $var_time_service_set_maxpoll \3/" "$config_file"
+done
+
